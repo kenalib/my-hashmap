b66e3951963b9ea3a4e6ac0f67bd762da82a4cdb
Ken <ken@example.com>
2018-08-29 00:53:21 +0900

    add test

---
+++ src/main/java/com/example/MyHashMap.java
@@ -0,0 +1,31 @@
+package com.example;
+
+public class MyHashMap {
+
+    int get(int key) {
+        return 0;
+    }
+
+    void put(int key, int value) {
+    }
+
+    boolean containsKey(int key) {
+        return true;
+    }
+
+    boolean isEmpty() {
+        return true;
+    }
+
+    void clear() {
+    }
+
+    int size() {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -16,5 +18,32 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void shouldWork() {
+        MyHashMap map = new MyHashMap();
+
+        assertTrue(map.isEmpty());
+        assertEquals(map.size(), 0);
+
+        map.put(1, 10);
+        map.put(2, 20);
+        map.put(3, 30);
+
+        assertFalse(map.isEmpty());
+        assertEquals(map.size(), 3);
+
+        assertTrue(map.containsKey(1));
+        assertFalse(map.containsKey(5));
+
+        assertEquals((int)map.get(1), 10);
+        assertEquals((int)map.get(2), 20);
+        assertEquals((int)map.get(3), 30);
+
+        map.clear();
+
+        assertTrue(map.isEmpty());
+        assertEquals(map.size(), 0);
     }
 }
